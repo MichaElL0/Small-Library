c6c00704c0b32c7cfd23f4b1bc097d488799c79e
mik <dkujoo@gmail.com>
2026-03-13 05:46:56 +0100

    Inistial project setup

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return (`${this.title} by ${this.author}, ${pages}, ${this.read}`);
+    }
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
