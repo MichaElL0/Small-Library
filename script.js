213e6d4d4d98a1acd03cd3f8df7fcb3c4953bbfe
mik <dkujoo@gmail.com>
2026-03-15 17:46:21 +0100

    Add function to add books to library

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
 
     this.info = function() {
-        return (`${this.title} by ${this.author}, ${pages}, ${this.read}`);
+        return (`${this.title} by ${this.author}, ${pages}, ${this.read} with ID of ${this.id}`);
     }
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
+
+addBookToLibrary("1984", "George Orwell", 384, "not read");
+
+console.log(myLibrary[0]);
+console.log(myLibrary[0].info())
